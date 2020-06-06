f1b264e4c7f94818cf89de87d661d261a2a7ac7f
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-06-06 23:46:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,10 @@ def oxford_comma(array)
 
 if array.length == 2
   array.join (" and ")
-end
+
 elsif array.lenght == 1
   array.join
-end
+
 elsif array > 2
   array[-1].prepend ("and")
   array.join (", ")
